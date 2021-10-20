9ae197b37ecd9c6a3adcabd80be983f4aa15fb50
diewland <diewland@gmail.com>
2021-10-20 20:50:28 +0700

    prepare UI for dev

--- app/src/main/java/com/diewland/writefiledemo/MainActivity.kt
+++ app/src/main/java/com/diewland/writefiledemo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.diewland.writefiledemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import com.diewland.log2file.Log2File
 
 class MainActivity : AppCompatActivity() {
@@ -13,24 +14,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // config
-        val path = "/sdcard/demo.txt"
-        val limit = 10
-        val round = 1_000
+        // button 1 --- fix line size
+        findViewById<Button>(R.id.btn_one).setOnClickListener {
+            // config
+            val path = "/sdcard/demo.txt"
+            val limit = 10
+            val round = 1_000
 
-        // init log
-        val l = Log2File(path, limit)
+            // init log
+            val l = Log2File(path, limit)
 
-        // before write
-        l.read().forEach { Log.d(tag, it) }
-        Log.d(tag, l.readText())
+            // before write
+            l.read().forEach { Log.d(tag, it) }
+            Log.d(tag, l.readText())
 
-        // write N round
-        for (i in 1..round) { l.write(i.toString()) }
+            // write N round
+            for (i in 1..round) { l.write(i.toString()) }
 
-        // after write
-        l.read().forEach { Log.d(tag, it) }
-        Log.d(tag, l.readText())
+            // after write
+            l.read().forEach { Log.d(tag, it) }
+            Log.d(tag, l.readText())
+        }
 
+        // button 2 --- diary log
+        findViewById<Button>(R.id.btn_two).setOnClickListener {
+            // TODO
+        }
     }
 }
